c6762ff047cd81a2726b1539de8f508b4c1f893e
Esteven <steven.undefined@gmail.com>
2021-03-15 20:02:45 -0500

    primera clase de javascript

---
+++ semana4/dia1/variables/codigo.js
@@ -0,0 +1,10 @@
+//este es un comentario de una linea
+/*
+este es un comentario multilineal*/
+// alert("Hola Munda")
+//variabel de tipo texto
+let nombre="Steven"
+let sistemaOperativo="Windons"
+console.log(typeof(nombre))//esto me devuelve string
+// alert("Mi nombre es" + nombre +"y mi sistema operativo que tengo es" + sistemaOperativo)
+alert(`¡Hola, mi nombre es: ${nombre}!`) //Interpolacion de cadenas
---
+++ semana4/script.js
@@ -0,0 +1,1 @@
+alert("Hola, yo me llamo Stv")
