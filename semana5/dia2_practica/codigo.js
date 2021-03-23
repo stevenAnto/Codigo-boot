d083874e7cfd2bf782a063a7721413dfe5d4affc
Esteven <steven.undefined@gmail.com>
2021-03-23 17:52:12 -0500

    Interfaz para calcular la sumatoria de una numero dado

--- semana5/dia2_practica/codigo.js
+++ semana5/dia2_practica/codigo.js
@@ -5,23 +5,48 @@ let parrafo=document.querySelector("p")
 //     alert(dias[i]);
 // }
 
-function nombre_dias() {
-    for(i in dias){
+// function nombre_dias() {
+//     for(i in dias){
         
-        p=dias;
-        parrafo.textContent=p
-    }
+//         p=dias;
+//         parrafo.textContent=p
+//     }
     
     
-}
+// }
 
-function presiona_boton() {
-    nombre_dias()
+// function presiona_boton() {
+//     nombre_dias()
     
+// }
+
+function pedir_un_numero() {
+    a=prompt("ingresa un numero");
+    suma_N_numeros(a)
+   
 }
 
 const boton=document.querySelector("button");
-boton.addEventListener("click",presiona_boton);
+boton.addEventListener("click",pedir_un_numero)
+
+// function creaMensaje() {
+//     var mensaje = "“Mensaje de prueba”";
+//     alert(mensaje)
+//   }
+// creaMensaje()
+
+function suma_N_numeros(n) {
+    let i=0;
+    let resultado=0
+    while(i<=n){
+        
+        resultado +=i;
+        i++
+    }
+    document.write(resultado)
+    
+}
+
 
 
 
